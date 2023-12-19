a6a8f6a99b94daa568a5368eea23025a12c6a25c
Hüseyin Atık <93039124+hsynaatik@users.noreply.github.com>
2023-12-19 22:31:31 +0300

    PlayerPositioning3D
    PlayerPositioning3D

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    private float lowerBound = -10;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over");
+            Destroy(gameObject);
+        }
+    }
+}
